67519c2144d1b5dfc42bbe8860bceb3238b4d805
Mopkovka13 <evgeniyneskin13@gmail.com>
2024-03-07 04:29:33 -0800

    add easy Get method

--- spring-boot-example/src/main/java/com/evgeniyA/Main.java
+++ spring-boot-example/src/main/java/com/evgeniyA/Main.java
@@ -2,10 +2,22 @@ package com.evgeniyA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// SpringBootApplication заменяет 3 интерфейса
+// @ComponentScan(basePackages="com.evgeniyA")
+// @Configuration
+// @EnableAutoConfiguration
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
     }
 }
